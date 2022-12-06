987d6efc2aa468328e2c7d7f273203f66cc2671b
Lauritz Holtmann <security@lauritz-holtmann.de>
2022-12-06 17:51:59 +0100

    Add launchURL feature

--- app/src/main/java/com/lauritz/oauthredirecturitest/MainActivity.java
+++ app/src/main/java/com/lauritz/oauthredirecturitest/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -24,5 +27,20 @@ public class MainActivity extends AppCompatActivity {
         alertDialog.setTitle("Test");
         alertDialog.setMessage("Action: " + action + "\nIntent-Data: " + data);
         alertDialog.show();
+
+        // Launch URL on Button Tap
+        final Button button = findViewById(R.id.launchUrl);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText simpleEditText = (EditText) findViewById(R.id.editTextUrl);
+                String url = simpleEditText.getText().toString();
+
+                if (!url.startsWith("http://") && !url.startsWith("https://"))
+                    url = "http://" + url;
+
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(browserIntent);
+            }
+        });
     }
 }
